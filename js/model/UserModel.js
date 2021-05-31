163f6be7c692148fbb2e8f3a7db764b11d1b1f6b
Diogo Malhão <dlmalhao@gmail.com>
2021-06-01 00:26:49 +0100

    index update

---
+++ js/model/UserModel.js
@@ -0,0 +1,13 @@
+export default class UserModel {
+    constructor(email, password, username, gender, age, exp, achieves, inventory, spintime) {
+        this.email = email
+        this.password = password
+        this.username = username
+        this.gender = gender
+        this.age = age
+        this.exp = exp
+        this.achieves = achieves
+        this.inventory = inventory
+        this.spintime = spintime
+    }
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+let form = document.querySelector('#register-form');
+
+// form.addEventListener('submit', function() {
+//     let x = document.querySelector('.form-check-input:checked').value;
+//     console.log(x)
+// })
+
+// form.addEventListener('submit', function() {
+//     let x = document.querySelector('#age-value').value
+//     console.log(x)
+// })
